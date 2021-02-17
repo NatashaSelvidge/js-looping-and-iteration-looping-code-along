6057669ea403900d2b5b0decee39ef87f50e2f41
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-02-16 16:43:33 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(namesArr, eventName) {
+  let newArr = []
+  for (let i = 0; i < namesArr.length; i++) {
+    newArr.push(`Thank you, ${namesArr[i]}, for the wonderful ${eventName} gift!`);
+  }  
+  return newArr;
+}
+
+
+function countDown(num) {
+  while (num >= 0) {
+      console.log(num--)
+  }
+  
+}
